cf3b23e0cc04106f14579899fcda2b3087a2bc19
michael-maton <mjmaton6@gmail.com>
2021-07-10 14:56:01 -0500

    one solution added for mostExpensive

--- Databases/alwaysOrderTables.sql
+++ Databases/alwaysOrderTables.sql
@@ -4,3 +4,10 @@ BEGIN
     FROM results
     ORDER BY wins ASC;
 END
+
+CREATE PROCEDURE mostExpensive()
+BEGIN
+	SELECT MIN(name) as name
+    FROM Products
+    WHERE price * quantity = (SELECT MAX(price * quantity) from Products);
+END
