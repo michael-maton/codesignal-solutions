401b3aeb26f3345adefb10245b2ea3829880e63d
michael-maton <mjmaton6@gmail.com>
2021-07-10 14:59:20 -0500

    second solution to mostExpensive

--- Databases/alwaysOrderTables.sql
+++ Databases/alwaysOrderTables.sql
@@ -7,7 +7,14 @@ END
 
 CREATE PROCEDURE mostExpensive()
 BEGIN
-	SELECT MIN(name) as name
+	SELECT MIN(name) as name /* MIN(name) lexicographically sorts names */
     FROM Products
     WHERE price * quantity = (SELECT MAX(price * quantity) from Products);
+END
+
+CREATE PROCEDURE mostExpensive()
+BEGIN
+	SELECT name 
+    FROM Products 
+    ORDER BY price * quantity DESC, name ASC LIMIT 1; /* lexicographically sorts names */
 END
