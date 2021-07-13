3c004d7ce0423108277d2058503b6b5e54f1c301
Michael Maton <mjmaton6@gmail.com>
2021-07-13 01:26:11 -0500

    better solution added for groupingDishes -- uses less memory

--- HashTables/groupingDishes.py
+++ HashTables/groupingDishes.py
@@ -24,3 +24,21 @@ def groupingDishes(dishes):
         ret.append(temp)
         
     return ret
+
+    
+def groupingDishes2(dishes):
+    table = {}
+    for dish in dishes:
+        for i in dish[1:len(dish)]:
+            if i not in table:
+                table[i] = [dish[0]]
+            else:
+                table[i].append(dish[0])
+                
+    ret = []
+    for key in sorted(table):
+        if len(table[key]) > 1:
+            ret.append([key] + sorted(table[key]))
+        
+    return ret
+    
