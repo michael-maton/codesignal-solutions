72e3b810c04d8631f0f1e314aed165ab1e2a4adb
Michael Maton <mjmaton6@gmail.com>
2021-07-29 16:37:03 -0500

    solution added for extraNumber, cool 1 liner if else example

--- TheCore/atTheCrossroads.py
+++ TheCore/atTheCrossroads.py
@@ -6,3 +6,6 @@ def knapsackLight(value1, weight1, value2, weight2, maxW):
         return value1 + value2
     else:
         return max(value1 if weight1 <= maxW else 0, value2 if weight2 <= maxW else 0)
+
+def extraNumber(a, b, c):
+    return a if b == c else b if a == c else c
