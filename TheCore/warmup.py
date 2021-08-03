2e6a7b0375ffdff1578fed7ccccbbb226093085c
Michael Maton <mjmaton6@gmail.com>
2021-08-02 23:40:35 -0500

    one-liner practice for makeArrayConsecutive2

--- TheCore/warmup.py
+++ TheCore/warmup.py
@@ -48,3 +48,9 @@ def shapeArea(n):
 def shapeArea2(n):
     return n**2 + (n-1)**2
 
+# For statues = [6, 2, 3, 8], the output should be
+# makeArrayConsecutive2(statues) = 3.
+# Needs statues of sizes 4, 5 and 7.
+
+def makeArrayConsecutive2(statues):
+    return (max(statues) - min(statues) + 1) - len(statues)
